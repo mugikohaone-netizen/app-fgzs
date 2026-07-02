9479e174753503730fe548865640bc70808181b9
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-02 21:51:51 +0900

    🔄 SW更新 vmr3i7e55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mr3glp5v';
+const CACHE = 'app-mr3i7e55';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
